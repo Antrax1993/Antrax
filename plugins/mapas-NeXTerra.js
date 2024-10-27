93f151a5273c5aa79c4d2f3002051308492e2dea
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 16:11:39 -0400

    Update mapas-NeXTerra.js

--- plugins/mapas-NeXTerra.js
+++ plugins/mapas-NeXTerra.js
@@ -25,6 +25,5 @@ global.db.data.users[m.sender].lastcofre = new Date * 1
 handler.help = ['nexterra']
 handler.tags = ['freefire']
 handler.command = ['nexterra'] 
-handler.register = true
 handler.admin = true
 export default handler
