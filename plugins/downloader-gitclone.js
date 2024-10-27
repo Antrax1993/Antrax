e76c2fa82a2317c85a5a749ec7650be1f19a0548
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 15:36:25 -0400

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -13,7 +13,6 @@ await conn.sendFile(m.chat, url, filename, null, m)
 }}
 handler.help = ['gitclone <url git>']
 handler.tags = ['downloader']
-handler.command = ['gitclone'] 
-handler.register = true 
+handler.command = ['gitclone']  
 //handler.limit = 1
 export default handler
