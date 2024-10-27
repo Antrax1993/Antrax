363c23700728e2f8348e3e94444ed41ff404a392
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 16:32:10 -0400

    Update tools-igstalk.js

--- plugins/tools-igstalk.js
+++ plugins/tools-igstalk.js
@@ -1,12 +1,12 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de usuario de Instagram.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* luisitocomunica`)
+if (!text) return m.reply('⚡ Ingresa el nombre de usuario de Instagram.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* luisitocomunica`)
 
 try {
 let { username, name, post, followers, following, bio, thumbnail, url } = await Scraper.igstalk(text)
 let txt = `╭─⬣「 *Instagram Stalk* 」⬣\n`
-    txt += `│  ≡◦ *🍭 Username ∙* ${username}\n`
+    txt += `│  ≡◦ *⭐ Username ∙* ${username}\n`
     txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
     txt += `│  ≡◦ *⚖ Posts ∙* ${post}\n`
     txt += `│  ≡◦ *🐢 Seguidores ∙* ${followers}\n`
@@ -20,5 +20,4 @@ await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
 handler.command = ['igstalk']
-handler.register = true 
 export default handler
