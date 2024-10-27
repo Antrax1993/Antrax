8cf8e120a4f96b67646e54f82d1bd454c10d9335
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 16:16:04 -0400

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
     let nn = conn.getName(m.sender);
      conn.reply('544123989549@s.whatsapp.net', `⭐ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
-    m.reply('⭐ Ocurrio un error.');
+    m.reply('Demasiado Bueno 👻');
   }
 };
 handler.tags = ['owner'];
