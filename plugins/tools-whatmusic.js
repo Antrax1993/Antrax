bb84104d4cdbf66e63759333d65352fda3de4ba6
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 16:51:34 -0400

    Update tools-whatmusic.js

--- plugins/tools-whatmusic.js
+++ plugins/tools-whatmusic.js
@@ -25,5 +25,4 @@ handler.help = ['whatmusic <audio/video>']
 handler.tags = ['tools']
 handler.command = /^(whatmusic|shazam)$/i
 //handler.limit = 1
-handler.register = true 
 export default handler
