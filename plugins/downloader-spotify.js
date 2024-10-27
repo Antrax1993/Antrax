e8022e2a33c2e0bc1d802e1987a56f90cc313228
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 15:02:49 -0400

    Update downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,13 +1,13 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de algún Track de Spotify.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`)
+if (!text) return m.reply('⭐ Ingresa el nombre de algún Track de Spotify.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`)
 
 let user = global.db.data.users[m.sender]
 try {
 let { title, artist, album, published, thumbnail, dl_url } = await Scraper.spotify(text)
 let txt = `╭─⬣「 *Spotify Download* 」⬣\n`
-    txt += `│  ≡◦ *🍭 Nombre ∙* ${title}\n`
+    txt += `│  ≡◦ *⭐ Nombre ∙* ${title}\n`
     txt += `│  ≡◦ *🪴 Artista ∙* ${artist}\n`
     txt += `│  ≡◦ *📚 Album ∙* ${album}\n`
     txt += `│  ≡◦ *📅 Publicado ∙* ${published}\n`
@@ -18,6 +18,5 @@ await conn.sendFile(m.chat, dl_url, title + '.mp3', `*🍭 Titulo ∙* ${title}\
 }}
 handler.help = ['spotify <búsqueda>']
 handler.tags = ['downloader']
-handler.command = ['spotify']
-handler.register = true 
+handler.command = ['spotify'] 
 export default handler
