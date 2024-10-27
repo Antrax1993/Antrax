75d3c2daf334bec2f71f3f4ed0afd505fa23396a
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 14:57:20 -0400

    Update img-neko.js

--- plugins/img-neko.js
+++ plugins/img-neko.js
@@ -12,6 +12,5 @@ await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', null, m)
 handler.help = ['neko']
 handler.tags = ['img']
 handler.command = ['neko']
-handler.register = true 
 //handler.limit = 1
 export default handler
