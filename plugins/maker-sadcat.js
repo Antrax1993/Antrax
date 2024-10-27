e4af804036d4f120280376a990fe394f8f9af376
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 14:41:21 -0400

    Update maker-sadcat.js

--- plugins/maker-sadcat.js
+++ plugins/maker-sadcat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 let response = args.join(' ').split('|')
-if (!text) return m.reply(`🍭 Ingresa un texto junto al comando.`)
+if (!text) return m.reply(`⭐ Ingresa un texto junto al comando.`)
 try {
 let res = `https://api.popcat.xyz/sadcat?text=${text}`
 await conn.sendFile(m.chat, res, 'thumbnail.jpg', null, m)
@@ -10,5 +10,4 @@ handler.help = ['sadcat <texto>']
 handler.tags = ['logo']
 handler.command = ['sadcat', 'catsad']
 //handler.limit = 1
-handler.register = true 
 export default handler
