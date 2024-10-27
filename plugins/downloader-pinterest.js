1cf23dd8d69b09fb187a49c8126c86bf1c3606e2
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-27 15:37:11 -0400

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -15,7 +15,6 @@ throw m.reply(`Error`)
 }}
 handler.tags = ['downloader']
 handler.help = ['pindl <pin url>']
-handler.command = /^(pindl)$/i
-handler.register = true 
+handler.command = /^(pindl)$/i 
 handler.limit = 1
 export default handler
