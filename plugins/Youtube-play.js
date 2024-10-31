8238e36aa1063c9a56abab0b24aa999f49a55815
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-30 23:35:53 -0400

    Update Youtube-play.js

--- plugins/Youtube-play.js
+++ plugins/Youtube-play.js
@@ -1,27 +1,24 @@
 import yts from 'yt-search' 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Ejemplo: ${usedPrefix + command} diles`,m ,rcanal;
-
     const randomReduction = Math.floor(Math.random() * 5) + 1;
     let search = await yts(text);
+    let f = `\n\n${String.fromCharCode(68,101,118,101,108,111,112,101,100,32,98,121,32,73,39,109,32,70,122,32,126)}`;
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
     let body = `\`YouTube Play\`
-
     *Título:* ${search.all[0].title}
     *Vistas:* ${search.all[0].views}
     *Duración:* ${search.all[0].timestamp}
     *Subido:* ${search.all[0].ago}
     *Url:* ${urls}
-
-🕐 *El ${isVideo ? 'Video' : 'Audio'} se está enviando, un momento...*`;
+🕒 *Su ${isVideo ? 'Video' : 'Audio'} se está enviando, espere un momento...*`;
     
     conn.sendMessage(m.chat, { 
         image: { url: search.all[0].thumbnail }, 
-        caption: body
+        caption: body + f
     }, { quoted: m,rcanal });
-    m.react('🎵')
-
+    m.react('react1')
     let res = await dl_vid(urls)
     let type = isVideo ? 'video' : 'audio';
     let video = res.data.mp4;
@@ -32,12 +29,10 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
         mimetype: isVideo ? "video/mp4" : "audio/mpeg" 
     }, { quoted: m });
 }
-
-handler.command = ['play', 'playvid'];
-handler.help = ['play <Busquedad>', 'playvid <Busquedad>'];
+handler.command = ['play <Busqueda>', 'playvid <Busquedad>'];
+handler.help = ['play', 'playvid'];
 handler.tags = ['dl'];
 export default handler;
-
 async function dl_vid(url) {
     const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
         method: 'POST',
@@ -50,11 +45,9 @@ async function dl_vid(url) {
             text: url,
         })
     });
-
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     const data = await response.json();
     return data;
-}
+    }
